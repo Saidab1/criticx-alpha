d369f449e4df54d1c78ee18a4c793b97e6766c39
saida brito <saidabrito57@gmail.com>
2020-05-26 12:36:54 -0500

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-    has_many :reviews 
+  has_many :reviews 
+  validates :username, :email, presence: true 
+  validate :birth_date_more_than_sixteen
+
+  def birth_date_more_than_sixteen
+    if p Date.today.year - birth_date.year  < 16
+      errors.add(:birth_date, "You should be 16 years old to create an account")
+    end
+  end 
+
 end

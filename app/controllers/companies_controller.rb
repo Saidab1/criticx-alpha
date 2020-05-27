b388af6cbb6ca23c92b36a086be877f366ac6142
saida brito <saidabrito57@gmail.com>
2020-05-27 11:57:06 -0500

    Task 4 Create a form to create new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,4 +7,22 @@ class CompaniesController < ApplicationController
     @companies = Company.find(params[:id])
     render :show
   end 
+
+  def new
+    @company = Company.new
+  end 
+
+  def create 
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path(@company)
+    else
+      render :new  
+    end
+  end 
+
+  private 
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
 end

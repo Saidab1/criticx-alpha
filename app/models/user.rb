69255f922039305ae38dc8b8f59a7874958d5efe
saida brito <saidabrito57@gmail.com>
2020-05-28 08:54:45 -0500

    Task 5. Implement authentication with devise on your Criticx project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :reviews 
   validates :username, :email, presence: true 
   validate :birth_date_more_than_sixteen
 
   def birth_date_more_than_sixteen
-    if p Date.today.year - birth_date.year  < 16
+    if Date.today.year - birth_date.year  < 16
       errors.add(:birth_date, "You should be 16 years old to create an account")
     end
   end 
